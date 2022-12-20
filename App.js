448d297bf3d69db897713eec38fcd4e458011d8e
TanmoyMistry <118536768+TanmoyMistry@users.noreply.github.com>
2022-12-20 16:57:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+
+// import Login from './screens/Login';
+// import Signup from './screens/Signup';
+
+import RootStack from './navigators/RootStack';
+
+export default function App() {
+  return <RootStack/>;
+}
+
